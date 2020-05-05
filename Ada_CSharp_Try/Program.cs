b8e08b306ff4f59ca72aedba326bca1fa0e3c50d
Patrick Abbs <patrickabbs@Patricks-MacBook-Pro.local>
2020-05-05 09:14:41 -0500

    initializing new cSharp implementation try for Ada

---
+++ Ada_CSharp_Try/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ada_CSharp_Try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
