53dc5f232a0786511fc2e6dde7a74c1df7631f7c
Patrick Abbs <patrickabbs@Patricks-MacBook-Pro.local>
2020-05-05 12:59:11 -0500

    add in database connection basics

---
+++ Ada_CSharp_Try/Persistence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Npgsql;
+
+namespace Ada_CSharp_Try
+{
+    public class Persistence
+    {
+        public Persistence()
+        {
+            var connection_string = "Host=localhost;Username=patrickabbs;Password=password;Database=ada_csharp";
+
+            using var connection = new NpgsqlConnection(connection_string);
+            connection.Open();
+
+            var sql = "SELECT version()";
+
+            using var cmd = new NpgsqlCommand(sql, connection);
+
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"PostgreSQL version: {version}");
+        }
+
+    }
+}
--- Ada_CSharp_Try/Program.cs
+++ Ada_CSharp_Try/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Ada_CSharp_Try.Classes;
+using Npgsql;
 
 namespace Ada_CSharp_Try
 {
@@ -8,6 +9,19 @@ namespace Ada_CSharp_Try
         static void Main(string[] args)
         {
             Classes.Dimension_Space Word_Space = new Dimension_Space("word", .4, .34, .03, 5.0, .8);
+
+            var connection_string = "Host=localhost;Username=patrickabbs;Password=password;Database=ada_csharp";
+
+            using var connection = new NpgsqlConnection(connection_string);
+            connection.Open();
+
+            var sql = "SELECT version()";
+
+            using var cmd = new NpgsqlCommand(sql, connection);
+
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"PostgreSQL version: {version}");
+
             Console.WriteLine(Word_Space.Space_Name);
             Console.WriteLine("Hello World!");
         }
